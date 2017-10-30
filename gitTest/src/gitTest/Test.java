3dd4dae7e676e190043def5a2c0c439096081680
sg <sg@70-12-111-50.pools.spcsdns.net>
2017-10-30 17:24:55 +0900

    커밋을시도

--- gitTest/src/gitTest/Test.java
+++ gitTest/src/gitTest/Test.java
@@ -4,6 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
 		System.out.println("추가작업을 하였습니다.");
-                System.out.println("딴놈이 한 작업");
+        System.out.println("딴놈이 한 작업");
+        System.out.println("나도 작업을 했고");
 	}
 }
