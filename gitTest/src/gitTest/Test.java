6a61a57d6de6f779b09bd5b83b816c8f25d3ef45
SeunggilHong <iccack70@gmail.com>
2017-10-30 17:20:25 +0900

    Update Test.java

--- gitTest/src/gitTest/Test.java
+++ gitTest/src/gitTest/Test.java
@@ -4,5 +4,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
 		System.out.println("추가작업을 하였습니다.");
+                System.out.println("딴놈이 한 작업");
 	}
 }
